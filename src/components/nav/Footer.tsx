e254dce74b531d43fba77dc758244e3aa7b09b2d
T_b <77137650+nullm0ose@users.noreply.github.com>
2026-03-03 11:44:59 -0800

    Added donations paypal hook & substack URL to footer

--- src/components/nav/Footer.tsx
+++ src/components/nav/Footer.tsx
@@ -4,8 +4,8 @@ import { FaHandshake } from "react-icons/fa6";
 
 export default function Footer() {
   const socialLinks = [
-    { label: "Support my work", href: "https://www.laurensickles.com", icon: <FaHandshake /> },
-    { label: "Follow me on Substack", href: "https://yourhandle.substack.com", icon: <FaPenFancy /> },
+    { label: "Support my work", href: "https://www.paypal.com/donate/?business=7BCYKC3SRNTTW&no_recurring=0&item_name=These+pages+are+free+to+read.+If+you%E2%80%99d+like+to+support+this+archive+and+future+work%2C+I%E2%80%99m+grateful.+One-time+or+ongoing.&currency_code=USD", icon: <FaHandshake /> },
+    { label: "Follow & Subscribe", href: "https://laurennsickles.substack.com", icon: <FaPenFancy /> },
   ];
 
   return (

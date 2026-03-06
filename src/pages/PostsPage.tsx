6b25aa99a137c469e156b5738c32df269425c558
T_b <77137650+nullm0ose@users.noreply.github.com>
2026-03-06 02:44:02 -0800

    Styled posts page to be reader friendly

--- src/pages/PostsPage.tsx
+++ src/pages/PostsPage.tsx
@@ -34,52 +34,68 @@ export default function PostPage() {
   const backLabel = type ? typeLabels[type] || "Posts" : "Posts";
 
   return (
-    <main className="max-w-3xl mx-auto px-6 py-12 sm:py-16 space-y-16">
+    <main className="max-w-5xl mx-auto px-7 py-12 sm:py-16 space-y-10 bg-white/60 backdrop-blur-lg rounded-xl">
 
-      {/* Header */}
-     <section className="text-center space-y-6">
-  <h1 className="
-    text-4xl sm:text-4xl md:text-5xl
-    font-instrument font-base
-    leading-tight tracking-tight
-    text-balance
-  ">
+{/* Header */}
+<section
+  className="
+    prose
+    max-w-none
+    text-left
+    prose-h1:mb-3
+    prose-p:mt-0
+    prose-h1:font-pro
+    prose-h1:font-medium
+    prose-h1:leading-normal
+    prose-h1:tracking-tight
+    prose-h1:text-foreground
+    prose-p:font-pro
+    prose-p:text-muted-foreground/85
+    prose-p:leading-normal
+  "
+>
+  <h1
+    className="
+      text-4xl sm:text-4xl md:text-5xl
+      text-balance
+    "
+  >
     {post.title}
   </h1>
 
   {post.excerpt && (
-    <p className="
-      max-w-2xl mx-auto
-      text-base sm:text-lg md:text-lg
-      leading-relaxed
-      text-foreground/60
-      
-    ">
+    <p className="text-base sm:text-lg md:text-lg max-w-3xl">
       {post.excerpt}
     </p>
   )}
 </section>
 
+    <div className="mx-auto max-w-5xl h-px bg-border opacity-70" />
+
+
       {/* Content */}
 <section
-  className=" backdrop-blur-sm 
-             border 
-             bg-card/10 border-border/30
-             rounded-md
-             px-4 sm:px-10 md:px-14
-             py-10 sm:py-14
-             shadow-sm"
+  className=" 
+         
+             py-5 sm:py-14 text-foreground max-w-4xl
+        "
 >
-  <div
-    className="space-y-6 sm:space-y-8
-               text-base sm:text-lg
-               leading-relaxed sm:leading-loose
-               text-left sm:text-left"
-  >
-    {post.content.map((paragraph, i) => (
-      <p key={i} className="mx-auto sm:mx-0 max-w-prose">
-        {paragraph}
-      </p>
+<div
+  className="
+    prose
+    sm:prose-lg
+    max-w-none
+    space-y-6 sm:space-y-8
+    text-left
+    prose-p:font-spectral
+    prose-p:text-foreground
+    prose-p:leading-loose
+  "
+>
+  {post.content.map((paragraph, i) => (
+    <p key={i} className="mx-auto sm:mx-0  ">
+      {paragraph}
+    </p>
     ))}
   </div>
 </section>
